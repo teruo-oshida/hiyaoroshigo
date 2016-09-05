c22534a70ec071e018073730bb2abc8a9225950f
Shugo Maeda <shugo@ruby-lang.org>
2016-09-05 21:17:56 +0900

    Add assertion to check SakeTemperature.enabled.count.

--- test/models/sake_temperature_test.rb
+++ test/models/sake_temperature_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SakeTemperatureTest < ActiveSupport::TestCase
   test "enabled returns only enabled records" do
+    assert_equal(3, SakeTemperature.enabled.count)
     SakeTemperature.enabled.each do |st|
       assert_equal(true, st.enabled)
     end
