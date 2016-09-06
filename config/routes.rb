7f1376338f6fc7b184fcc1403683786c5e4bfc17
Shugo Maeda <shugo@ruby-lang.org>
2016-09-06 10:24:45 +0900

    Add routes. See https://github.com/matsueoss/hiyaoroshigo/wiki/%E3%81%AA%E3%82%93%E3%81%A8%E3%81%AA%E3%81%8FURL%E8%A8%AD%E8%A8%88 for details.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :drinkers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :terms, only: [:show]
+  get :signup, to: "devise/registrations#new"
+  resources :festivals, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :sakes, only: [:index]
+    resources :checkins, only: [:index, :create]
+  end
+  resources :drinkings, only: [:index, :new, :create, :delete]
+  resource :drinker, only: [:show, :edit, :update]
+  get :itsitoolate, to: "festivals#itstoolate"
+  get :thewinner, to: "festivals#thewinner"
 end
