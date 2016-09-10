5ae53033fbb823ed23c62df539d2bddb80dbd27f
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-10 16:39:35 +0900

    Add Method find_for_facebook_oauth in drinker model

--- app/models/drinker.rb
+++ app/models/drinker.rb
@@ -5,4 +5,18 @@ class Drinker < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  def self.find_for_facebook_oauth(auth)
+    drinker = Drinker.where(provider: auth.provider, uid: auth.uid).first
+    
+    unless drinker
+      drinker = Drinker.create( name:     auth.extra.raw_info.name,
+                                provider: autch.provider,
+                                uid:      auth.uid,
+                                email:    auth.info.email,
+                                token:    auth.credentials.token,
+                                password: Devise.friendly_token[0.20] )
+    end
+
+    return drinker
+  end
 end
