20936542f29724532b1c60cd330a68053ed8c078
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-10 20:14:39 +0900

    Modify drinker in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :drinkers
+  devise_for :drinker, :controllers => { :omniauth_callbacks => "drinker/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :terms, only: [:show]
   get :signup, to: "devise/registrations#new"
