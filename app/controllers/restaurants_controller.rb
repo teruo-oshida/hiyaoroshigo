fd357a23bae758316488521b9707cef6895624a8
Shugo Maeda <shugo@ruby-lang.org>
2016-09-11 03:06:11 +0900

    Use sake.png.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,6 +8,11 @@ class RestaurantsController < ApplicationController
     @markers = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
       marker.lat restaurant.latitude
       marker.lng restaurant.longitude
+      marker.picture({
+        url: "/assets/sake.png",
+        width: 32,
+        height: 32
+      })
       max = restaurant.tickets.count
       current = 
         restaurant.checkins.where("created_at > ?", 60.minutes.ago).count
