835c654895a42decfb6617eb2a55cbab1ee34416
ShotaOnishi <showta.0804@gmail.com>
2016-09-11 00:45:47 +0900

    checkinテーブルへのデータ保存

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,4 +1,6 @@
 class CheckinsController < ApplicationController
+   before_action :authenricate_drinker!
+
   def new
     @checkin = Checkin.new
     @restaurants = Restaurant.all
@@ -6,6 +8,8 @@ class CheckinsController < ApplicationController
 
   def create
     restaurant_id = params[:restaurant][:id]
+    user_id = current_drinker.id
+    Checkin.create(restaurant_id: restaurant_id, drinker_id: user_id)
     render text: "#{Restaurant.find(restaurant_id).name}にチェックイン"
   end
 end
