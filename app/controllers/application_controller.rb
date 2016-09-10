e6f8b4122d121055cfbe38b9b47a4e56fe1fe3d0
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-10 16:41:04 +0900

    Overwrite authenticate_user! in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenricate_user!
+    session[:user_return_to] = env['PATH_INFO']
+    redirect_to user_omniauth_authorize_patu(:facebook) unless user_signed_in?
+  end
 end
