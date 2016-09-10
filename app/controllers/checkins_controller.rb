e0cd06a0a56dfbe4e8ce7d0b1dced722bb0c3556
ShotaOnishi <showta.0804@gmail.com>
2016-09-10 21:56:52 +0900

    チェックインページを作成

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,2 +1,10 @@
 class CheckinsController < ApplicationController
+  def new
+    @checkin = Checkin.new
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    render text: "#{Restaurant.find(params[:restaurant_id]).name}にチェックイン"
+  end
 end
