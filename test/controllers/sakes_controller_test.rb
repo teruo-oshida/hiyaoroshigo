65da36027cd62519d17abd965bdcbc4970567eb4
Shugo Maeda <shugo@ruby-lang.org>
2016-09-11 11:35:39 +0900

    不要なテストの削除。

--- test/controllers/sakes_controller_test.rb
+++ test/controllers/sakes_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class SakesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get sakes_index_url
-    assert_response :success
-  end
-
 end
