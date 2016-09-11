53d4b694c11d6fc8b89d62a51f27eb51ad9c2413
Shugo Maeda <shugo@ruby-lang.org>
2016-09-11 18:48:43 +0900

    画像の中央寄せ。

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :restaurant_participations
   has_many :festivals, through: :restaurant_participations
   has_many :sake_menu_items
+  has_many :sakes, through: :sake_menu_items
   has_many :drinkings
   has_many :checkins
   has_many :tickets
