a0d4fb0c4892cb3b08ee584a346f9c91a6360ea5
Shugo Maeda <shugo@ruby-lang.org>
2016-09-11 09:29:40 +0900

    マーカークリック時にAjaxで/restaurants/:idからデータを取得するよう修正。

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,11 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    render html: @restaurant.name
+  end
+
   def map
     @restaurants = Restaurant.all
     @markers = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
@@ -13,10 +18,11 @@ class RestaurantsController < ApplicationController
         width: 32,
         height: 32
       })
-      max = restaurant.tickets.count
-      current = 
-        restaurant.checkins.where("created_at > ?", 60.minutes.ago).count
-      marker.infowindow "#{restaurant.name} (#{current}/#{max})"
+      # max = restaurant.tickets.count
+      # current = 
+      #   restaurant.checkins.where("created_at > ?", 60.minutes.ago).count
+      # marker.infowindow "#{restaurant.name} (#{current}/#{max})"
+      marker.title restaurant.id.to_s
     end
   end
 end
