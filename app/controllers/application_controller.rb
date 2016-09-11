16c868b654ccd05e240fc53a7e78eff48086c27b
Yamato Mitsui <yamato.mitsui.orleans@gmail.com>
2016-09-11 00:35:41 +0000

    Store vote and drinking db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def authenticate_drinker!
-    session[:drinker_return_to] = env['PATH_INFO']
-    redirect_to drinker_facebook_omniauth_authorize_path unless drinker_signed_in?
-  end
+  #def authenticate_drinker!
+  #  session[:drinker_return_to] = env['PATH_INFO']
+  #  redirect_to drinker_facebook_omniauth_authorize_path unless drinker_signed_in?
+  #end
 end
--- app/controllers/drinkings_controller.rb
+++ app/controllers/drinkings_controller.rb
@@ -1,20 +1,28 @@
 class DrinkingsController < ApplicationController
-  def new
+  def index
     @drinking = Drinking.new
     @vote = Vote.new
   end
 
+  def new
+    @drinking = Drinking.create(sake_id: params[:sake_id], drinker_id: current_drinker.id)
+    @vote = Vote.create(drinking_id: @drinking.id, score: 0)
+  end
+
   def create
-    @drinking = Drinking.create(drinking_params)
+   # @drinking = Drinking.create(drinking_params)
     @vote = Vote.create(vote_params)
+  end
+
+  def show
   end
 
   private
     def drinking_params
-      params.require(:drinking).permit()
+      params.require(:drinking).permit(:sake_id)
     end
 
     def vote_params
-      params.reqiore(:vote).permit(:drinking_id, :score)
+      params.require(:vote).permit(:drinking_id, :score)
     end
 end
