0d551e0c87d934c5b4425727c2ce71dc567b39e4
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-09-13 00:44:36 +0900

    Add accepts_nested_attributes_for

--- app/models/drinking.rb
+++ app/models/drinking.rb
@@ -1,8 +1,9 @@
 class Drinking < ApplicationRecord
-  belongs_to :festival
   belongs_to :drinker
+  belongs_to :festival
   belongs_to :restaurant
   belongs_to :sake
   belongs_to :sake_temperature
   has_one :vote
+  accepts_nested_attributes_for :vote
 end
