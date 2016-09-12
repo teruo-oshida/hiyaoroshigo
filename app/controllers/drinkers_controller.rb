d9b77a5dfc1341a07a41c4614924b5ec8b015c47
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-12 16:53:43 +0900

    Modify Redirect Path in drinkers_controller

--- app/controllers/drinkers_controller.rb
+++ app/controllers/drinkers_controller.rb
@@ -6,7 +6,7 @@ class DrinkersController < ApplicationController
   end
 
   def login
-    
+
   end
 
   def edit
@@ -22,6 +22,6 @@ class DrinkersController < ApplicationController
     drinker = current_drinker
     drinker.name = params[:drinker][:name]
     drinker.save
-    redirect_to :action => "show"
+    redirect_to "/drinkers/edit"
   end
 end
