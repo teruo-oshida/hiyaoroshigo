f44071e3827592c57320c55083a7129ce457a8ca
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-13 21:14:02 +0900

    Modify result format

--- app/controllers/drinker/omniauth_callbacks_controller.rb
+++ app/controllers/drinker/omniauth_callbacks_controller.rb
@@ -6,21 +6,21 @@ class Drinker::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # 紐づくticketレコードを生成してやれば良いかと
     @drinker = Drinker.find_for_facebook_oauth(request.env["omniauth.auth"],request.env['omniauth.params'])
 
-    if @drinker.persisted?
+    if @drinker[:status] && @drinker[:drinker].persisted?
       redirect_by_checkin_status
-      sign_in @drinker
+      sign_in @drinker[:drinker]
     else
      logger.debug("failed!!")
      # session["devise.facebook_data"] = request.env["omniauth.auth"]
-     # redirect_to new_drinker_registration_url
+     redirect_to login_path;
     end
   end
 
   private
 
   def redirect_by_checkin_status
-    if @drinker.checked_in?
-      restaurant_id = @drinker.checked_in_restaurant_id
+    if @drinker[:drinker].checked_in?
+      restaurant_id = @drinker[:drinker].checked_in_restaurant_id
       redirect_to restaurant_path(restaurant_id)
     else
       redirect_to new_checkin_path
