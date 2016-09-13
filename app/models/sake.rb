adc48418ab0517e79899db57e506ce40200df4c6
Shugo Maeda <shugo@ruby-lang.org>
2016-09-13 22:51:35 +0900

    障子の画像を適用。

--- app/models/sake.rb
+++ app/models/sake.rb
@@ -3,7 +3,7 @@ class Sake < ApplicationRecord
   has_many :sake_menu_items
   has_many :drinkings
 
-  has_attached_file :photo, styles: { medium: "400x400#", thumb: "150x150#" }
+  has_attached_file :photo, styles: { medium: "588x412#", thumb: "150x150#" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   def voted?(drinker)
