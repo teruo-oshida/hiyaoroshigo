c95ef70056ecc38c13ffcdfad589c9d6c4bd3f3f
Shugo Maeda <shugo@ruby-lang.org>
2016-09-14 22:44:54 +0900

    festival_idを改竄できないように修正。 (fixes #99)

--- app/controllers/drinkings_controller.rb
+++ app/controllers/drinkings_controller.rb
@@ -7,14 +7,14 @@ class DrinkingsController < ApplicationController
     @drinking = Drinking.new
     @drinking.build_vote
 
-    @festival_id   = Festival.current&.take&.id
     @restaurant_id = params[:restaurant_id]
     @sake_id       = params[:sake_id]
     @temperatures  = SakeTemperature.enabled
   end
 
   def create
-    drinking = Drinking.new(drinking_params.merge(drinker: current_drinker))
+    drinking = Drinking.new(drinking_params.merge(drinker: current_drinker,
+                                                  festival: current_festival))
     drinking.save!
 
     @restaurant = drinking.restaurant
@@ -43,7 +43,7 @@ class DrinkingsController < ApplicationController
 
   def drinking_params
     params.require(:drinking)
-          .permit(:festival_id, :restaurant_id, :sake_id, :sake_temperature_id,
+          .permit(:restaurant_id, :sake_id, :sake_temperature_id,
                   vote_attributes: :score)
   end
 end
