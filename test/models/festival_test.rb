7cf5479f7d756259e0360aff28e88a810eb1727e
Shugo Maeda <shugo@ruby-lang.org>
2016-09-14 16:40:51 +0900

    テストが間違っていたため修正。

--- test/models/festival_test.rb
+++ test/models/festival_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class FestivalTest < ActiveSupport::TestCase
   test "started?" do
     f = Festival.new(start_at: 1.second.since)
-    assert(false, f.started?)
+    assert_equal(false, f.started?)
     f = Festival.new(start_at: Time.current)
-    assert(true, f.started?)
-    f = Festival.new(start_at: 1.second.before)
-    assert(true, f.started?)
+    assert_equal(true, f.started?)
+    f = Festival.new(start_at: 1.second.ago)
+    assert_equal(true, f.started?)
   end
 end
