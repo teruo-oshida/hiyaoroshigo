7b8b6fff49201515e2a8e02eba332b6a5ef8e9ac
Sota Yano <kuroisiratama@yahoo.co.jp>
2016-09-15 07:19:18 +0900

    Refactor method

--- app/models/drinker.rb
+++ app/models/drinker.rb
@@ -11,34 +11,19 @@ class Drinker < ApplicationRecord
   # TODO: パスフレーズを受け取ってticketレコードを紐付ける処理を追加する
   def self.find_for_facebook_oauth(auth,params)
     drinker = Drinker.where(provider: auth.provider, uid: auth.uid).first
-    error = ""
-    status = false
-    status = true if drinker
-    unless drinker
-      ticket = Ticket.find_by(passcode: params["passcode"])
-      if auth.info.email.present?
-        if ticket.present?
-          if ticket.unused?
-            ticket.build_drinker(full_name:     auth.extra.raw_info.name,
-                                 provider: auth.provider,
-                                 uid:      auth.uid,
-                                 email:    auth.info.email,
-                                 token:    auth.credentials.token,
-                                 password: Devise.friendly_token)
-            ticket.save!
-            status = true
-            drinker = ticket.drinker
-          else
-            error = "既に使われているパスコードです"
-          end
-        else
-          error = "存在しないパスコードです"
-        end
-      else
-        error = "メールアドレスが取得できませんでした。スタッフにお問い合わせください"
-      end
-    end
-    {status: status, drinker: drinker, error: error}
+    return { status: true, drinker: drinker } if drinker
+    return { status: false, error: "メールアドレスが取得できませんでした。スタッフにお問い合わせください" } unless auth.info.email.present?
+    ticket = Ticket.find_by(passcode: params["passcode"])
+    return { status: false, error: "存在しないパスコードです" } unless ticket.present?
+    return { status: false, error: "既に使われているパスコードです" } unless ticket.unused?
+    ticket.build_drinker(full_name: auth.extra.raw_info.name,
+                         provider:  auth.provider,
+                         uid:       auth.uid,
+                         email:     auth.info.email,
+                         token:     auth.credentials.token,
+                         password:  Devise.friendly_token)
+    ticket.save!
+    { status: true, drinker: ticket.drinker }
   end
 
   def checked_in?
